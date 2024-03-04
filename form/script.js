06bd6c87ea6c8ae29dd76369d5e66b367c64fd72
deepakthakur9199 <imdeepak752@gmail.com>
2024-03-04 06:44:54 -0800

    form

---
+++ form/script.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var destination = document.getElementById("destination").value;
+    var date = document.getElementById("date").value;
+    var timing = document.getElementById("timing").value;
+    
+    // Do something with the form data (e.g., send it to a server)
+    console.log("Name:", name);
+    console.log("Destination:", destination);
+    console.log("Date:", date);
+    console.log("Timing:", timing);
+  }
+  
